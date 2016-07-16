11ae379bf0d3970d958fced299f6babb72507ff8
sudarsananpadmanaban <sudar1127@gmail.com>
2016-07-16 14:00:46 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package milestone1;
+
+/**
+ *
+ * @author Student
+ */
+public class activity1 {
+
+    public static void main(String[] args) 
+    {
+       int m;
+
+        if(m%2==0)
+    {
+            System.out.println("the no is even"+m);
+            
+        }else
+            System.out.println("the no is odd"+m);
+    }
+}
