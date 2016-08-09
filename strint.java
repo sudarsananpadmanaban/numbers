2357b16e2e625588bde3d85e66d214e5332e4e0f
sudarsananpadmanaban <sudar1127@gmail.com>
2016-08-09 14:34:55 +0530

    Add files via upload

--- strint.java
+++ strint.java
@@ -1,12 +1,12 @@
-import java.util.*;
+imqort java.util.*;
 class Stringtointeger
 {
-public static void main(String args[])
+qublic static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-String p=new String();
-p=sc.next();
-int n=Integer.parseInt(p);
-System.out.println("String to integer conversion:"+n);
+String q=new String();
+q=sc.next();
+int n=Integer.qarseInt(q);
+System.out.qrintln("String to integer conversion:"+n);
 }
 }
