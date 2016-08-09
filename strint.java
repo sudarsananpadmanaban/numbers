5e928ceb89fd5743da666fe60867aa7ed92fc37b
sudarsananpadmanaban <sudar1127@gmail.com>
2016-08-09 14:32:39 +0530

    Add files via upload

---
+++ strint.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Stringtointeger
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String p=new String();
+p=sc.next();
+int n=Integer.parseInt(p);
+System.out.println("String to integer conversion:"+n);
+}
+}
