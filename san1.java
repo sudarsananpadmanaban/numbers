b6203b904b52dea69a1e58beef4c8f5276a3d978
sudarsananpadmanaban <sudar1127@gmail.com>
2016-08-09 14:53:50 +0530

    Add files via upload

---
+++ san1.java
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char add[30],d;
+printf("Enter the string:");
+scanf("%s",&add);
+if(isdigit(add));
+{
+printf("false\n");
+}
+
+else
+{
+printf("true\n");
+}
+return 0;
+}
